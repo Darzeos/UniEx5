446cacc93320c9ba120da5ed7c5ae78c5d7acf0a
DarkbladeZeo <roman.abstoss@outlook.com>
2024-06-06 12:59:26 +0200

    Cleaned mergesort

--- mergesort.py
+++ mergesort.py
@@ -1,51 +1,68 @@
-def ASSIGNMENT(new_list, i, old_list, j):
-    new_list[i] = old_list[j]
-
-
-def mergeSort(list_to_sort_by_merge):
-    if (
-        len(list_to_sort_by_merge) > 1
-        and not len(list_to_sort_by_merge) < 1
-        and len(list_to_sort_by_merge) != 0
-    ):
-        mid = len(list_to_sort_by_merge) // 2
-        left = list_to_sort_by_merge[:mid]
-        right = list_to_sort_by_merge[mid:]
-
-        mergeSort(left)
-        mergeSort(right)
-
-        l = 0
-        r = 0
-        i = 0
-
-        while l < len(left) and r < len(right):
-            if left[l] <= right[r]:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=left, j=l)
-                l += 1
-            else:
-                ASSIGNMENT(new_list=list_to_sort_by_merge, i=i, old_list=right, j=r)
-                r += 1
-            i += 1
-
-        while l < len(left):
-            list_to_sort_by_merge[i] = left[l]
-            l += 1
-            i += 1
-
-        while r < len(right):
-            list_to_sort_by_merge[i] = right[r]
-            r += 1
-            i += 1
-
-
+#6. Moved import to top of code
 import matplotlib.pyplot as plt
 
-my_list = [54, 26, 93, 17, 77, 31, 44, 55, 20]
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
-mergeSort(my_list)
-x = range(len(my_list))
-plt.plot(x, my_list)
-plt.show()
+#1. Changed name of function better represent workings
+#Assigns an element from old_list[j] to new_list[i]
+def assign_element(new_list, i, old_list, j):
+    new_list[i] = old_list[j]
+
+#2. Shortend parameter name
+#merge sort body
+def merge_sort(arr):
+    #3. Removed redundanten checks and renamed variables
+    if len(arr) > 1:
+        mid = len(arr) // 2
+        left_half = arr[:mid]
+        right_half = arr[mid:]
+
+        # Recursively sort both halves
+        merge_sort(left_half)
+        merge_sort(right_half)
+
+        #4. Renamed indexes for better recognition
+        left_index = 0
+        right_index = 0
+        sorted_index = 0
+
+        # Merge the sorted halves
+        while left_index < len(left_half) and right_index < len(right_half):
+            if left_half[left_index] <= right_half[right_index]:
+                assign_element(arr, sorted_index, left_half, left_index)
+                left_index += 1
+            else:
+                assign_element(arr, sorted_index, right_half, right_index)
+                right_index += 1
+            sorted_index += 1
+
+        # Copy any remaining elements of left_half
+        while left_index < len(left_half):
+            arr[sorted_index] = left_half[left_index]
+            left_index += 1
+            sorted_index += 1
+
+        # Copy any remaining elements of right_half
+        while right_index < len(right_half):
+            arr[sorted_index] = right_half[right_index]
+            right_index += 1
+            sorted_index += 1
+
+#5. Extracted plot functions to create seperate plots via parameter to remove code doubling
+def plot_list(arr, title):
+    x = range(len(arr))
+    plt.plot(x, arr)
+    plt.title(title)
+    plt.show()
+
+
+#7. Renamed some variables
+# Example usage
+myList = [54, 26, 93, 17, 77, 31, 44, 55, 20]
+    
+# Plot original list
+plot_list(myList, 'Original List')
+    
+# Sort the list using merge sort
+merge_sort(myList)
+    
+# Plot sorted list
+plot_list(myList, 'Sorted List')
